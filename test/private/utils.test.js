d049faad714533673c423dbfa05a38d4f54608bf
daikiueda <daikiueda.1979@gmail.com>
2014-03-29 21:15:35 +0900

    ユーティリティ・メソッドの仕様を変更するので、テストを先に直した。

--- test/private/utils.test.js
+++ test/private/utils.test.js
@@ -1,84 +1,67 @@
 var expect = require( "chai" ).expect,
-    sinon = require( "sinon" ),
-    rewire = require( "rewire" ),
-
-    utilModulePath = "../../lib/private/utils.js",
-    utils = require( utilModulePath );
+    utils = require( "../../lib/private/utils.js" );
 
 describe( "private functions / utils", function(){
 
-    describe( "evalColumnPosition", function(){
-        var evalColumnPosition = utils.evalColumnPosition;
-
-        it( "引数が数値の場合、その数値を返却する。", function(){
-            expect( evalColumnPosition( 5 ) ).to.equal( 5 );
-            expect( evalColumnPosition( "5" ) ).to.equal( 5 );
-        } );
+    describe( "toOrdinalNumber", function(){
+        var toOrdinalNumber = utils.toOrdinalNumber;
 
         it( "引数がアルファベットの場合、数値に変換して返却する。（A=1, B=2 ... Z=26, AA=27）", function(){
-            expect( evalColumnPosition( "A" ) ).to.equal( 1 );
-            expect( evalColumnPosition( "a" ) ).to.equal( 1 );
-            expect( evalColumnPosition( "Z" ) ).to.equal( 26 );
-            expect( evalColumnPosition( "AA" ) ).to.equal( 27 );
-            expect( evalColumnPosition( "BA" ) ).to.equal( 53 );
+            expect( toOrdinalNumber( "A" ) ).to.equal( 1 );
+            expect( toOrdinalNumber( "a" ) ).to.equal( 1 );
+            expect( toOrdinalNumber( "Z" ) ).to.equal( 26 );
+            expect( toOrdinalNumber( "AA" ) ).to.equal( 27 );
+            expect( toOrdinalNumber( "BA" ) ).to.equal( 53 );
         } );
 
-        it( "引数がない、または数値かアルファベットでない場合、Errorを投げる。", function(){
-            expect( function(){ evalColumnPosition(); } ).to.throw( Error );
-            expect( function(){ evalColumnPosition( "あ" ); } ).to.throw( Error );
+        it( "引数が整数の場合、その数値を返却する。", function(){
+            expect( toOrdinalNumber( 5 ) ).to.equal( 5 );
+            expect( toOrdinalNumber( "5" ) ).to.equal( 5 );
         } );
 
-        it( "引数はone-basedの列位置なので、0が与えられた場合は、Errorを投げる。", function(){
-            expect( function(){ evalColumnPosition( 0 ); } ).to.throw( Error );
+        it( "引数がない、またはアルファベットか整数でない場合、Errorを投げる。", function(){
+            expect( function(){ toOrdinalNumber(); } ).to.throw( Error );
+            expect( function(){ toOrdinalNumber( 1.5 ); } ).to.throw( Error );
+            expect( function(){ toOrdinalNumber( "あ" ); } ).to.throw( Error );
+        } );
+
+        it( "引数で0が与えられた場合は、Errorを投げる。", function(){
+            expect( function(){ toOrdinalNumber( 0 ); } ).to.throw( Error );
         } );
     } );
 
-    describe( "evalColumnIndex", function(){
-        var evalColumnIndex = utils.evalColumnIndex;
+    describe( "toOrdinalAlphabet", function(){
+        var toOrdinalAlphabet = utils.toOrdinalAlphabet;
 
-        it( "引数で与えられたone-basedの列位置を、zero-basedに変換して返却する。", function(){
-            expect( evalColumnIndex( 5 ) ).to.equal( 4 );
-            expect( evalColumnIndex( 'AA' ) ).to.equal( 26 );
+        it( "引数が整数の場合、アルファベットに変換して返却する。（1=A, 2=B ... 26=Z, 27=AA）", function(){
+            expect( toOrdinalAlphabet( 1 ) ).to.equal( "A" );
+            expect( toOrdinalAlphabet( "1" ) ).to.equal( "A" );
+            expect( toOrdinalAlphabet( 26 ) ).to.equal( "Z" );
+            expect( toOrdinalAlphabet( 27 ) ).to.equal( "AA" );
+            expect( toOrdinalAlphabet( 53 ) ).to.equal( "BA" );
         } );
 
-        describe( "引数が過去に与えられた値と同じ場合、_.memoizeがキャッシュした値を返却する。", function(){
+        it( "引数がアルファベットの場合、その文字列を返却する。", function(){
+            expect( toOrdinalAlphabet( "AA" ) ).to.equal( "AA" );
+        } );
 
-            it( "evalColumnIndexを実行する前は、evalColumnPositionは呼ばれていない。", function(){
-                var rewiredUtilModule = rewire( utilModulePath ),
-                    spyEvalColumnPosition = sinon.spy( rewiredUtilModule.__get__( "evalColumnPosition" ) );
+        it( "引数に含まれる小文字のアルファベットは、大文字に変換して返却する。", function(){
+            expect( toOrdinalAlphabet( "Aa" ) ).to.equal( "AA" );
+        } );
 
-                rewiredUtilModule.__set__( "evalColumnPosition", spyEvalColumnPosition );
+        it( "引数がない場合、Errorを投げる。", function(){
+            expect( function(){ toOrdinalAlphabet(); } ).to.throw( Error );
+            expect( function(){ toOrdinalAlphabet( "あ" ); } ).to.throw( Error );
+        } );
 
-                expect( spyEvalColumnPosition.called ).to.be.false;
-            } );
+        it( "引数が整数またはアルファベットのみでない場合、Errorを投げる。", function(){
+            expect( function(){ toOrdinalAlphabet( 1.5 ); } ).to.throw( Error );
+            expect( function(){ toOrdinalAlphabet( "あ" ); } ).to.throw( Error );
+            expect( function(){ toOrdinalAlphabet( "A9" ); } ).to.throw( Error );
+        } );
 
-            it( "evalColumnIndexを2度同じ引数で実行しても、2度目のevalColumnPositionは呼ばれていない。", function(){
-                var rewiredUtilModule = rewire( utilModulePath ),
-                    evalColumnIndex = rewiredUtilModule.__get__( "evalColumnIndex" ),
-                    spyEvalColumnPosition = sinon.spy( rewiredUtilModule.__get__( "evalColumnPosition" ) );
-
-                rewiredUtilModule.__set__( "evalColumnPosition", spyEvalColumnPosition );
-
-                evalColumnIndex( 8 );
-                evalColumnIndex( 8 );
-
-                expect( spyEvalColumnPosition.called ).to.be.true;
-                expect( spyEvalColumnPosition.calledTwice ).to.be.false;
-            } );
-
-            it( "evalColumnIndexを2度、異なる引数で実行すると、evalColumnPositionは2度実行される。", function(){
-                var rewiredUtilModule = rewire( utilModulePath ),
-                    evalColumnIndex = rewiredUtilModule.__get__( "evalColumnIndex" ),
-                    spyEvalColumnPosition = sinon.spy( rewiredUtilModule.__get__( "evalColumnPosition" ) );
-
-                rewiredUtilModule.__set__( "evalColumnPosition", spyEvalColumnPosition );
-
-                evalColumnIndex( 10 );
-                evalColumnIndex( 11 );
-
-                expect( spyEvalColumnPosition.called ).to.be.true;
-                expect( spyEvalColumnPosition.calledTwice ).to.be.true;
-            } );
+        it( "引数で0が与えられた場合は、Errorを投げる。", function(){
+            expect( function(){ toOrdinalAlphabet( 0 ); } ).to.throw( Error );
         } );
     } );
 } );

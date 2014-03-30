d7cf23668cd3cd8859fcb36d240111d1a1accf74
daikiueda <daikiueda.1979@gmail.com>
2014-03-31 03:19:39 +0900

    細かい仕様を検討しつつテストを書いてる。

--- test/xlsx2json.test.js
+++ test/xlsx2json.test.js
@@ -3,7 +3,7 @@ var expect = require( "chai" ).expect,
 
 describe( "xlsx2json( xlsxFilePath, [options], [callback] )", function(){
 
-    describe( "arguments", function(){
+    describe( "Arguments", function(){
         it( "xlsx2json( xlsxFilePath )", function( done ){
             xlsx2json( "test/xlsx/data_only.xlsx" ).done( function( result ){
                 expect( result ).to.deep.equal( [
@@ -15,30 +15,33 @@ describe( "xlsx2json( xlsxFilePath, [options], [callback] )", function(){
         } );
 
         it( "xlsx2json( xlsxFilePath, options )" /*, function( done ){
-            xlsx2json( "test/xlsx/data_only.xlsx", {} ).done( function( result ){
-                expect( result ).to.be.an( "array" );
-                done();
-            } );
-        }*/ );
+         xlsx2json( "test/xlsx/data_only.xlsx", {} ).done( function( result ){
+         expect( result ).to.be.an( "array" );
+         done();
+         } );
+         }*/ );
 
         it( "xlsx2json( xlsxFilePath, callback )" /*, function( done ){
-            xlsx2json( "test/xlsx/data_only.xlsx", function( error, result ){
-                expect( result ).to.be.an( "array" );
-                done();
-            } );
-        }*/ );
+         xlsx2json( "test/xlsx/data_only.xlsx", function( error, result ){
+         expect( result ).to.be.an( "array" );
+         done();
+         } );
+         }*/ );
 
         it( "xlsx2json( xlsxFilePath, options, callback )" /*, function( done ){
-            xlsx2json( "test/xlsx/data_only.xlsx", {}, function( error, result ){
-                expect( result ).to.be.an( "array" );
-                done();
-            } );
-        }*/ );
+         xlsx2json( "test/xlsx/data_only.xlsx", {}, function( error, result ){
+         expect( result ).to.be.an( "array" );
+         done();
+         } );
+         }*/ );
 
         it( "* If ary argument is invalid, an error will be thrown.", function(){
             expect( function(){ xlsx2json(); } ).to.throw( Error );
         } );
 
+        describe( "xlsxFilePath", function(){
+            it( "ファイル指定" );
+        } );
 
         describe( "options", function(){
             describe( "mapping: {Object}", function(){
@@ -50,8 +53,8 @@ describe( "xlsx2json( xlsxFilePath, [options], [callback] )", function(){
                             columnB: 2,
                             columnC: "3"
                         } },
-                        function( error, jsonArray ){
-                            expect( jsonArray ).to.deep.equal( [
+                        function( error, jsonValueArray ){
+                            expect( jsonValueArray ).to.deep.equal( [
                                 { "columnA": "value of A1", "columnB": "value of B1", "columnC": "value of C1" },
                                 { "columnA": "value of A2", "columnB": "value of B2", "columnC": "value of C2" }
                             ] );
@@ -61,13 +64,14 @@ describe( "xlsx2json( xlsxFilePath, [options], [callback] )", function(){
                 } );
             } );
 
-            describe( "keysRow: {Number}", function(){
+            describe( "keysRow: {Number} (*one-based row position)", function(){
                 it( "指定された行の値が、得られるオブジェクトのkey名になる。", function( done ){
                     xlsx2json(
                         "test/xlsx/with_keys_row.xlsx",
                         { keysRow: 1 },
-                        function( error, jsonArray ){
-                            expect( jsonArray ).to.deep.equal( [
+                        function( error, jsonValueArray ){
+                            //expect( jsonValueArray[ 0 ] ).to.have.keys( "columnA", "column B", "column-C" );
+                            expect( jsonValueArray ).to.deep.equal( [
                                 { "columnA": "value 1-A", "column B": "value 1-B", "column-C": "value 1-C" },
                                 { "columnA": "value 2-A", "column B": "value 2-B", "column-C": "value 2-C" }
                             ] );
@@ -76,14 +80,59 @@ describe( "xlsx2json( xlsxFilePath, [options], [callback] )", function(){
                     );
                 } );
 
-                it( "指定された行は、返却されるデータから除外される。" );
+                it( "指定された行は、返却されるデータから除外される。", function( done ){
+                    xlsx2json(
+                        "test/xlsx/with_keys_row.xlsx",
+                        { keysRow: 1 },
+                        function( error, jsonValueArray ){
+                            expect( jsonValueArray ).to.have.length( 2 );
+                            done();
+                        }
+                    );
+                } );
 
-                it( "mappingとkeysRowが両方とも指定された場合は、mappingが優先される。" );
+                describe( "If both mapping and keysRow is specified.", function(){
+                    it( "aaa" );
+//                    it( "", function( done ){
+//                        xlsx2json(
+//                            "test/xlsx/with_keys_row.xlsx",
+//                            {
+//                                keysRow: 1,
+//                                mapping: {
+//                                    columnA: "A",
+//                                    columnB: 2,
+//                                    columnC: "3"
+//                                }
+//                            },
+//                            function( error, jsonValueArray ){
+//                                console.log(jsonValueArray);
+//                            expect( jsonValueArray ).to.deep.equal( [
+//                                { "columnA": "columnA", "columnB": "column B", "columnC": "column-C" },
+//                                { "columnA": "value of A1", "columnB": "value of B1", "columnC": "value of C1" },
+//                                { "columnA": "value of A2", "columnB": "value of B2", "columnC": "value of C2" }
+//                            ] );
+//                                done();
+//                            }
+//                        );
+//                    } );
+                } );
+
             } );
 
-            describe( "dataStartingRow: {Number}", function(){
+            describe( "dataStartingRow: {Number} (*one-based row position)", function(){
                 it( "指定された行が、内容を取得する最初の位置になる。" );
             } );
+        } );
+
+        describe( "callback", function(){
+            it( "function( error, jsonValueArray ){ ... }" );
+        } );
+    } );
+
+    describe( "Returns", function(){
+        describe( "promise (*Q promise - http://documentup.com/kriskowal/q/)", function(){
+            it( ".done( function( jsonValueArray ){ ... } )" );
+            it( ".fail( function( error ){ ... } )" );
         } );
     } );
 } );
